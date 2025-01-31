ab63a3464271e0736f20bafed6c486115659c382
Nagy <nagada88@gmail.com>
2025-01-31 02:31:52 +0100

    elso elem bugfix

--- app_bettirelax/views.py
+++ app_bettirelax/views.py
@@ -4,8 +4,8 @@ from .models import *
 # Create your views here.
 
 def introduction(request):
-    aboutme = AboutMe.objects.get(id=1)
-    contact = Contact.objects.get(id=1)
+    aboutme = AboutMe.objects.first() 
+    contact = Contact.objects.first() 
     services = Service.objects.all()
     context={'aboutme': aboutme, 'services': services, 'contact': contact}
 
@@ -14,23 +14,23 @@ def introduction(request):
 def service(request, slug):
     service = get_object_or_404(Service, slug=slug)
     services = Service.objects.all()
-    contact = Contact.objects.get(id=1)
+    contact = Contact.objects.first() 
     context = {'service': service, 'services': services, 'contact': contact}
 
     return render(request, 'service.html', context)
 
 def pricelist(request):
     services = Service.objects.prefetch_related('prices').all()  
-    contact = Contact.objects.get(id=1)
+    contact = Contact.objects.first() 
     context = {'services': services, 'contact': contact}
                
     return render(request, 'pricelist.html', context)
 
 def faq(request):
     faqs = Faq.objects.all()
-    aboutme = AboutMe.objects.get(id=1)
+    aboutme = AboutMe.objects.first() 
     services = Service.objects.all()
-    contact = Contact.objects.get(id=1)
+    contact = Contact.objects.first() 
     context = {'faqs': faqs, 'services': services, 'contact': contact, 'aboutme': aboutme}
 
     return render(request, 'faq.html', context)
@@ -38,14 +38,14 @@ def faq(request):
 def bloglist(request):
     services = Service.objects.all()
     bloglist = BlogPost.objects.all()
-    contact = Contact.objects.get(id=1)
+    contact = Contact.objects..first() 
     context = {'services': services, 'bloglist': bloglist, 'contact': contact}
 
     return render(request, 'bloglist.html', context)
 
 def blogpost(request, slug):
     services = Service.objects.all()
-    contact = Contact.objects.get(id=1)
+    contact = Contact.objects.first() 
     context = {'blogpost': blogpost, 'services': services, 'contact': contact}
 
     return render(request, 'blogpost.html', context)
